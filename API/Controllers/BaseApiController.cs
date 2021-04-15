df218eeae07ff51f406ddf5d14976363fed78ce7
Nadelaide <nataliepworden@gmail.com>
2021-04-14 22:43:32 -0400

    End of Section 10

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,10 +1,11 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
-    [ApiController]
+    [ApiController] // automatically creates http error responses
     [Route("api/[controller]")] 
     public class BaseApiController : ControllerBase
     {
@@ -12,5 +13,15 @@ namespace API.Controllers
 
                                 // ??= means, if _mediator is null, assign the left to that property
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result) 
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
     }
 }
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -8,12 +10,20 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; } //what we want to receive
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command> //command class contains the activity
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator()); //uses Activity Validator to validate if items are empty or null
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -21,13 +31,15 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity); //adding activity in context in memory, not in database - async not required
 
-                await _context.SaveChangesAsync(); 
+                var result = await _context.SaveChangesAsync() > 0; //if nothing has been written to the database this will be false
 
-                return Unit.Value; //equivalent to nothing, lets API controller know we're finished.
+                if (!result) return Result<Unit>.Failure("Failed to create activiity");
+
+                return Result<Unit>.Success(Unit.Value); //equivalent to nothing, lets API controller know we're finished.
             }
         }
     }
